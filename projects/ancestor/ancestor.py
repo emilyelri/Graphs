9a102ae737bd4f6a05518af3cb2430e8c143d3f5
Emily_Richard <richard.emily.e@gmail.com>
2020-03-17 22:02:34 -0400

    ancestors mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,51 @@
+from graph import Graph
+from util import Queue
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    g = ancestors_graph(ancestors)
+
+    if len(g.get_neighbors(starting_node)) == 0:
+        return -1
+
+    distance = 0
+    distances = {}
+    
+    q = Queue()
+    q.enqueue([starting_node, distance])
+
+    while q.size() > 0:
+        anc_set = q.dequeue()
+        person = anc_set[0]
+        curr_distance = anc_set[1]
+
+        if curr_distance in distances:
+            distances[curr_distance].append(person)
+
+        else:
+            distances[curr_distance] = [person]
+
+        neighbors = g.get_neighbors(person)
+        if len(neighbors) > 0:
+            distance += 1
+            for relative in neighbors:
+                q.enqueue([relative, distance])
+    
+    farthest = max(distances.keys())
+    result = min(distances[farthest])
+
+    return result
+
+def ancestors_graph(ancestors):
+    g = Graph()
+    for ancestor in ancestors:
+        parent = ancestor[0]
+        child = ancestor[1]
+        if parent not in g.vertices:
+            g.add_vertex(parent)
+        if child not in g.vertices:
+            g.add_vertex(child)
+        g.add_edge(child, parent)
+    return g
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+ancestors_graph(test_ancestors)
